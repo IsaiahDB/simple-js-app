f36db541121c901610b9ee2b557808e47d9d1b64
IsiahDB <IsaiahBanks500@gmail.com>
2022-03-01 21:35:58 -0600

    changed big pokemon

--- js/script.js
+++ js/script.js
@@ -5,11 +5,6 @@ let pokemonList = [
     { name: 'Butterfree', height: 0.5, abilities: [ 'compoundeyes', 'tinted-lens'], weight: 32, type: 'Flight'},
     { name: 'Blastoise', height: 1.6, abilities: ['rain-dish', 'torrent'], weight: 85.5, type: 'Water'}];
 
-//This is just a simple out of the first pokemonList
-// for(let i = 0; i < pokemonList.length; i++){
-//     document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) <br>`)
-// }
-
 document.write("<br>")
 
 //ForEach pokemonList
@@ -68,10 +63,12 @@ document.write("<br>")
 document.write("<br>")
 
 //This is a for loop to show which pokemon is bigger
-for(let i = 0; i < pokemonList.length; i++){
-    if(pokemonList[i].height > 1.0){
-        document.write(`${pokemonList[i].name} height:${pokemonList[i].height} - Wow, that’s big! <br>`)
+function bigPokemon(itemP) {
+    if(itemP.height > 1.0){
+        document.write(`${itemP.name} height:${itemP.height} - Wow, that’s big! <br>`)
     }else{
-        document.write(`${pokemonList[i].name} height: ${pokemonList[i].height} <br>`)
+        document.write(`${itemP.name} height: ${itemP.height} <br>`)
     }
 }
+
+pokemonList.forEach(bigPokemon)
