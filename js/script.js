b3f3901ae09652d5ce3d8dab0dcaf11096d7315e
IsiahDB <IsaiahBanks500@gmail.com>
2022-03-02 18:08:00 -0600

    init commit

--- js/script.js
+++ js/script.js
@@ -9,10 +9,6 @@ document.write("<br>")
 
 //IFFE Function Style
 let pokemonRepository = (function () {
-    let pokemonList = [
-        { name: 'Growlithe', height: 0.7, abilities: ['flash-fire', 'Intimidate', 'Justified'], weight: 19, type: 'Fire'},
-        { name: 'Butterfree', height: 0.5, abilities: [ 'compoundeyes', 'tinted-lens'], weight: 32, type: 'Flight'},
-        { name: 'Blastoise', height: 1.6, abilities: ['rain-dish', 'torrent'], weight: 85.5, type: 'Water'}];
 
     function getAll(){
         return pokemonList
@@ -36,7 +32,7 @@ let pokemonRepository = (function () {
 pokemonRepository.add({name: 'Pikachu', height: 0.9} );
 console.log(pokemonRepository.getAll());
 console.log("Seperated")
-pokemonList = pokemonRepository.getAll();
+pokemonRepository.getAll();
 pokemonList.forEach((item, i) => {
     if(pokemonList[i].height >= 1.5){
         document.write((pokemonList[i].name) + " (height:" + (pokemonList[i].height) +") - Wow, that's big!"+"</br></br>");
@@ -45,8 +41,4 @@ pokemonList.forEach((item, i) => {
     }
 })
 
-
-//Line breaks or spaces to break up the output
-document.write("<br>")
-document.write("<br>")
 
